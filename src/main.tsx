cb9f0492032355032ada785d3f816632a4904a50
FukeKazki <kazkichi0906@gmail.com>
2023-09-02 15:55:05 +0900

    🚧  Firebase の導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,32 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
+import { initializeApp } from 'firebase/app';
 import App from './app/app';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
