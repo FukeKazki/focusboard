157717ff0b14b7b3790e81bd1981a294f03508d5
FukeKazki <kazkichi0906@gmail.com>
2023-09-02 15:22:06 +0900

    🚧 fix: lint

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
 import { LibSharedUi } from 'lib/shared/ui';
 
 export function App() {
   return (
     <div>
-      <LibSharedUi/>
-      <NxWelcome title="focusboard" />
+      <p>hello</p>
+      <LibSharedUi />
     </div>
   );
 }
