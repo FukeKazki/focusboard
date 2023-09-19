40a4dcb7d1eddf3b607db852e8b7ba59ecba87b1
FukeKazki <kazkichi0906@gmail.com>
2023-09-20 01:42:23 +0900

    ✨ boardsを取得

--- lib/shared/ui/src/lib/layout/layout.tsx
+++ lib/shared/ui/src/lib/layout/layout.tsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import Header from '../header/header';
 // import './layout.css';
 type Props = {
   children?: React.ReactNode;
+  boards: Array<{
+    id: string;
+    name: string;
+  }>;
 };
-export function Layout({ children }: Props) {
+export function Layout({ children, boards }: Props) {
   return (
     <div className="drawer">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
@@ -15,44 +20,27 @@ export function Layout({ children }: Props) {
         <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 min-h-full relative bg-base-200">
           <li className="menu-title">ボード</li>
-          <li>
-            <p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5"
-                />
-              </svg>
-              マイタスク
-            </p>
-          </li>
-          <li>
-            <p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5"
-                />
-              </svg>
-              読書
-            </p>
-          </li>
+          {boards.map((board, index) => (
+            <li key={`${board.id}-${index}`}>
+              <Link to={`dashboard/${board.id}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5"
+                  />
+                </svg>
+                {board.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,43 @@
+import { collection, getDocs } from 'firebase/firestore';
 import { Layout } from 'lib/shared/ui';
 import { Outlet } from 'react-router-dom';
+import { firestore } from '../main';
+import { User } from 'firebase/auth';
+import useSWR from 'swr';
+import { Board, getUserAsync } from './dashboard/dashboard-page';
+// TODO: custom hook にしたい
+async function fetchBoards(user: User) {
+  const boardRef = collection(
+    firestore,
+    'workspaces',
+    user?.uid ?? '',
+    'boards'
+  );
+  const boardSnapshots = await getDocs(boardRef);
+  const boards = await Promise.all(
+    boardSnapshots.docs.map(async (doc) => {
+      return { id: doc.id, ...doc.data() } as Board;
+    })
+  );
+  return boards;
+}
 
 export function App() {
+  const { data } = useSWR(
+    'boards',
+    async () => {
+      const user = await getUserAsync();
+      if (!user) {
+        return;
+      }
+      return fetchBoards(user);
+    },
+    {
+      revalidateOnFocus: false,
+    }
+  );
   return (
-    <Layout>
+    <Layout boards={data ?? []}>
       <Outlet />
     </Layout>
   );
